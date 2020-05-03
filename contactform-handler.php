fa2fca5404e3a72381ea8dc67f5e9e315010fef3
shafiqdaud <42223451+shafiqdaud@users.noreply.github.com>
2020-05-03 21:11:20 +0500

    Add files via upload

---
+++ contactform-handler.php
@@ -0,0 +1,26 @@
+<?php
+    $Name = $_POST['name'];
+    $visitor_email=$_POST['email'];
+    $message=$_POST['message'];
+
+    $email_from="https://github.com/shafiqdaud/web";
+    $email_subject="Suggestion/complaint";
+
+    $email_body= "User Name : $Name .\n",
+                    "User Email: $visitor_email .\n",
+                    "User Message: $message .\n";
+
+    $to="shafiqdaud49@gmail.com";
+
+    $headers ="From: $email_from \r\n";
+
+    $headers ="Reply-To: $visitor_email \r\n ";
+
+    mail($to, $email_subject,$email_body,$headers);
+
+    header("location: contactform.html");
+
+
+
+
+?>
